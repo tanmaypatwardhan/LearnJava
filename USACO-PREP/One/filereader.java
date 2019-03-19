ae1bd67dcc8ba5a9ec47e61e9c75ec2514a32ba6
Tanmay Patwardhan <tanmay.xvii@gmail.com>
2019-03-18 22:57:03 -0700

    Commit

---
+++ USACO-PREP/One/filereader.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class filereader {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new FileReader("FamilyNames.in"));
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("FamilyNames.out")));
+
+		for(int i = 0; i < 3 ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String firstname  = st.nextToken("/");
+			String lastname = st.nextToken("/");
+			pw.println("Lastname: " + lastname + "Firstname: " + firstname);
+		}
+
+		pw.close();
+		br.close();
+	}
+}
--- USACO-PREP/filereader.java
+++
@@ -1,7 +0,0 @@
-import java.io.*;
-import java.util.*;
-public class filereader {
-  public static void main(String[] args) {
-    System.out.println("Hello World\n");
-  }
-}
