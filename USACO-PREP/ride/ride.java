ada963770f533f2183153663da0031bc7a4fc72f
Tanmay Patwardhan <tanmay.xvii@gmail.com>
2019-03-19 22:12:27 -0700

    Commiting for today

---
+++ USACO-PREP/ride/ride.java
@@ -0,0 +1,14 @@
+/*
+ID: your_id_here
+LANG: JAVA
+PROG: ride
+*/
+import java.io.*;
+import java.util.*;
+public class ride {
+    public static void main(String[] args) {
+      char alphabet = 'Z';
+      int num_value_of_alphabet = (int) alphabet;
+      System.out.println(num_value_of_alphabet - 64);
+    }
+}
