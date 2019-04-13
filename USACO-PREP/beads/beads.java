f563fd6edafa0cf9b0f10fe9f7ee99008d90c53c
tanmaypatwardhan <tanmay.xvii@gmail.com>
2019-04-12 20:06:52 -0700

    Commiting

--- USACO-PREP/beads/beads.java
+++ USACO-PREP/beads/beads.java
@@ -16,29 +16,39 @@ public class beads {
     char[] beads_array = beads_string.toCharArray();
 
     Vector<Integer> streak_array = new Vector<Integer>();
+    Vector<Integer> white_array = new Vector<Integer>();
 
     int streak = 1;
+    int w_strk = 1;
     int streak_index=0;
     streak_array.add(1);
     for(int i = 1; i < num_beads; i++){
-        if(beads_array[i-1] == beads_array[i] || beads_array[i-1] == 'w'|| beads_array[i] == 'w') {   // Increment streak at the same index//
+        if(beads_array[i-1] == beads_array[i] ){ //beads_array[i-1] == 'w'   || beads_array[i] == 'w')    // Increment streak at the same index//
           streak++;
           streak_array.set(streak_index, streak);
         } else if(i==num_beads-1){                  // Take care if the last element changes in the streak//
           streak_index++;
           streak_array.add(1);
+          if(beads_array[i] == 'w' )
         } else {
           streak_index++;                          // Increment index when streak changes and a zero to at the new index position//
           streak_array.add(1);
           streak=1;
         }
+        if(beads_array[i - 1] == 'w' ){
+          w_strk++;
+          white_array.add(w_strk);
         }
+
+
+
+  }
+
      System.out.println(streak_array);
 
      for(int i = 1; i < streak_array.size() ; i++){
        if(streak_array.size() == 1){
          highest_strk = streak_array.get(0);
-
        } else {
          int sum = streak_array.get(i - 1) + streak_array.get(i);
          if(sum > highest_strk){
