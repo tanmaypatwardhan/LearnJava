58eef66f95fffe9588b6df683cd5b64268fc555f
tanmaypatwardhan <tanmay.xvii@gmail.com>
2019-06-19 23:09:24 -0700

    commit

--- USACO-BRONZE/2016/SquarePasture/SquarePasture.java
+++ USACO-BRONZE/2016/SquarePasture/SquarePasture.java
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import java.io.*;
-import java.util.*;
-public class SquarePasture {
-  public static void main(String[] args) throws IOException {
-  BufferedReader br = new BufferedReader(new FileReader("6.in"));
-  PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("SquarePasture.out")));
-
-  StringTokenizer st = new StringTokenizer(br.readLine());
-  int x1 = Integer.parseInt(st.nextToken());
-  int y1 = Integer.parseInt(st.nextToken());
-  int x2 = Integer.parseInt(st.nextToken());
-  int y2 = Integer.parseInt(st.nextToken());
-
-  st = new StringTokenizer(br.readLine());
-  int x3 = Integer.parseInt(st.nextToken());
-  int y3 = Integer.parseInt(st.nextToken());
-  int x4 = Integer.parseInt(st.nextToken());
-  int y4 = Integer.parseInt(st.nextToken());
-
-  if(Math.abs(y4 - y1) > Math.abs(x2 - x3)){
-    pw.println((Math.abs(y4 - y1)) * (Math.abs(y4 - y2)));
-  } else {
-      pw.println((Math.abs(x3 - x2)) * (Math.abs(x3 - x2)));
-  }
-
-  pw.close();
-  br.close();
-=======
 import java.util.*;
 import java.io.*;
 
@@ -46,12 +17,6 @@ public class squarePasture {
     int y3 = Integer.parseInt(st.nextToken());
     int x4 = Integer.parseInt(st.nextToken());
     int y4 = Integer.parseInt(st.nextToken());
-
-
-
-
-
-
 
     if(y2 > y4){
       if(x3 > x1){
@@ -78,7 +43,5 @@ public class squarePasture {
 
     pw.close();
     br.close();
-
->>>>>>> bde4264f083fa4731531b74f8756542989e4ff0d
   }
 }
